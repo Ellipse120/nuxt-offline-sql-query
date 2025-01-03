fc6268d1a012fcb1cdc536ada6e078e3810e27c8
Ellipse120 <ellipse120@gmail.com>
2025-01-03 14:11:43 +0800

    Init commit

---
+++ utils/index.ts
@@ -0,0 +1,16 @@
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+import type { Updater } from "@tanstack/vue-table";
+
+function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+function valueUpdater<T extends Updater<any>>(updaterOrValue: T, ref: Ref) {
+  ref.value =
+    typeof updaterOrValue === "function"
+      ? updaterOrValue(ref.value)
+      : updaterOrValue;
+}
+
+export { cn, valueUpdater };
